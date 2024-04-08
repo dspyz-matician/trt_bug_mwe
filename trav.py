f41ec2ed3485d33fdbc3ee9b70a86fccdbfa3c22
David Spies <david.spies@matician.com>
2024-04-08 09:00:26 -0700

    bug4

--- trav.py
+++ trav.py
@@ -10,37 +10,7 @@ MAX_TRAV = 21
 
 class Traversability(nn.Module):
     def forward(self, untraversable: torch.Tensor) -> torch.Tensor:
-        device = untraversable.device
-        sq_distances = torch.where((untraversable == 1) | (untraversable == 2), 0, MAX_TRAV**2)
-        extra_col = torch.full(
-            (sq_distances.shape[0], sq_distances.shape[1], 1),
-            MAX_TRAV**2,
-            device=device,
-            dtype=torch.int32,
-        )
-        for d in range(1, MAX_TRAV * 2 + 1, 2):
-            sq_distances = torch.minimum(
-                sq_distances,
-                torch.minimum(
-                    torch.cat([sq_distances[:, :, 1:] + d, extra_col], 2),
-                    torch.cat([extra_col, sq_distances[:, :, :-1] + d], 2),
-                ),
-            )
-        extra_row = torch.full(
-            (sq_distances.shape[0], 1, sq_distances.shape[2]),
-            MAX_TRAV**2,
-            device=device,
-            dtype=torch.int32,
-        )
-        for d in range(1, MAX_TRAV * 2 + 1, 2):
-            sq_distances = torch.minimum(
-                sq_distances,
-                torch.minimum(
-                    torch.cat([sq_distances[:, 1:, :] + d, extra_row], 1),
-                    torch.cat([extra_row, sq_distances[:, :-1, :] + d], 1),
-                ),
-            )
-        return sq_distances.sqrt()
+        return untraversable.to(torch.int32)
 
 
 # Create an instance of the model
